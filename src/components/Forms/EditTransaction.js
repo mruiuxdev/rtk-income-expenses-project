2fcb418b965239e1355fd668ce1a039f7d0cdb51
M-Ramadan <mr.uiux.dev@gmail.com>
2023-12-21 09:50:08 +0200

    transacation

--- src/components/Forms/EditTransaction.js
+++ src/components/Forms/EditTransaction.js
@@ -1,26 +1,44 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import {
+  editTransactionAction,
+  getTransactionAction,
+} from "../../redux/slice/transactions/transactions.slice";
 
 const EditTransaction = () => {
+  const dispatch = useDispatch();
+
+  const { transactions } = useSelector((state) => state);
+
+  const { id } = useParams();
+
+  const navigate = useNavigate();
+
   const [transaction, setTransaction] = useState({
-    title: "",
+    name: "",
     amount: "",
     transactionType: "",
-    date: "",
     category: "",
     notes: "",
   });
-  //---Destructuring---
-  const { title, amount, transactionType, date, category, notes } = transaction;
-  //---onchange handler----
+
+  const { name, amount, transactionType, category, notes } = transaction;
+
   const onChange = (e) => {
     setTransaction({ ...transaction, [e.target.name]: e.target.value });
   };
 
-  //---onsubmit handler----
   const onSubmit = (e) => {
     e.preventDefault();
+    dispatch(editTransactionAction({ ...transaction, id }));
+    navigate(`/dashboard`);
   };
+
+  useEffect(() => {
+    dispatch(getTransactionAction(id));
+  }, [dispatch, id]);
+
   return (
     <section className="py-16 overflow-hidden bg-white xl:pb-56">
       <div className="container px-4 mx-auto">
@@ -31,12 +49,17 @@ const EditTransaction = () => {
           <p className="mb-12 text-lg font-medium leading-normal text-gray-600">
             You are editing transaction to .....
           </p>
+          {transactions?.error?.message && (
+            <div className="mb-4 bg-red-100 text-red-800 rounded p-2">
+              {transactions?.error.message}
+            </div>
+          )}
           <form onSubmit={onSubmit}>
             <label className="block mb-5">
               <input
-                value={title}
+                value={transactions?.transaction?.data?.name}
                 onChange={onChange}
-                name="title"
+                name="name"
                 className="px-4 py-3.5 w-full text-gray-500 font-medium placeholder-gray-500 bg-white outline-none border border-gray-300 rounded-lg focus:ring focus:ring-indigo-300"
                 id="signUpInput2-1"
                 type="text"
@@ -45,7 +68,7 @@ const EditTransaction = () => {
             </label>
             <label className="block mb-5">
               <input
-                value={amount}
+                value={transactions?.transaction?.data?.amount}
                 onChange={onChange}
                 name="amount"
                 className="px-4 py-3.5 w-full text-gray-500 font-medium placeholder-gray-500 bg-white outline-none border border-gray-300 rounded-lg focus:ring focus:ring-indigo-300"
@@ -56,9 +79,9 @@ const EditTransaction = () => {
             </label>
             <label className="block mb-5">
               <select
-                value={category}
+                value={transactions?.transaction?.data?.transactionType}
                 onChange={onChange}
-                name="category"
+                name="transactionType"
                 className="appearance-none block w-full py-3 px-4 leading-tight text-gray-700 bg-gray-200 focus:bg-white border border-gray-200 focus:border-gray-500 rounded focus:outline-none"
               >
                 <option>-- Select Transaction Type --</option>
@@ -68,9 +91,9 @@ const EditTransaction = () => {
             </label>
             <label className="block mb-5">
               <select
-                value={transactionType}
+                value={transactions?.transaction?.data?.category}
                 onChange={onChange}
-                name="transactionType"
+                name="category"
                 className="appearance-none block w-full py-3 px-4 leading-tight text-gray-700 bg-gray-200 focus:bg-white border border-gray-200 focus:border-gray-500 rounded focus:outline-none"
               >
                 <option>-- Select Category --</option>
@@ -80,21 +103,11 @@ const EditTransaction = () => {
               </select>
             </label>
 
-            <label className="block mb-5">
-              <input
-                value={date}
-                onChange={onChange}
-                name="date"
-                className="px-4 py-3.5 w-full text-gray-500 font-medium placeholder-gray-500 bg-white outline-none border border-gray-300 rounded-lg focus:ring focus:ring-indigo-300"
-                id="signUpInput2-2"
-                type="date"
-              />
-            </label>
             <div>
               <div className="mt-3 mb-3">
                 <textarea
                   onChange={onChange}
-                  value={notes}
+                  value={transactions?.transcation?.data?.notes}
                   placeholder="Add Notes"
                   rows={4}
                   name="notes"
@@ -104,10 +117,11 @@ const EditTransaction = () => {
               </div>
             </div>
             <button
+              disabled={transactions?.loading ? true : false}
               type="submit"
               className="w-full py-4 mb-8 font-semibold text-white transition duration-200 ease-in-out bg-indigo-600 border border-indigo-700 px-9 rounded-xl shadow-4xl focus:ring focus:ring-indigo-300 hover:bg-indigo-700"
             >
-              Create Transaction
+              {transactions?.loading ? "Editing..." : "Edit Transaction"}
             </button>
             {/* <p className="font-medium">
               <span>Already have an account?</span>
--- src/redux/slice/transactions/transactions.slice.js
+++ src/redux/slice/transactions/transactions.slice.js
@@ -42,6 +42,61 @@ export const createTransactionAction = createAsyncThunk(
   }
 );
 
+export const editTransactionAction = createAsyncThunk(
+  "transaction/edit",
+  async (
+    { id, name, amount, transactionType, category, notes },
+    { rejectWithValue, getState, dispatch }
+  ) => {
+    try {
+      const token = getState()?.users?.userAuth?.userInfo?.token;
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      const { data } = await axios.put(
+        `${apiUrl}/transactions/${id}`,
+        {
+          name,
+          amount,
+          transactionType,
+          category,
+          notes,
+        },
+        config
+      );
+
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const getTransactionAction = createAsyncThunk(
+  "transaction/get",
+  async (id, { rejectWithValue, getState, dispatch }) => {
+    try {
+      const token = getState()?.users?.userAuth?.userInfo?.token;
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      const { data } = await axios.get(`${apiUrl}/transactions/${id}`, config);
+
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
 const transactionsSlice = createSlice({
   name: "transactions",
   initialState,
@@ -59,6 +114,38 @@ const transactionsSlice = createSlice({
       .addCase(createTransactionAction.rejected, (state, action) => {
         state.loading = false;
         state.isAdded = false;
+        state.transaction = null;
+        state.error = action.payload;
+      });
+
+    builder
+      .addCase(editTransactionAction.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(editTransactionAction.fulfilled, (state, action) => {
+        state.loading = false;
+        state.isUpdated = true;
+        state.transaction = action.payload;
+        state.error = null;
+      })
+      .addCase(editTransactionAction.rejected, (state, action) => {
+        state.loading = false;
+        state.isUpdated = false;
+        state.transaction = null;
+        state.error = action.payload;
+      });
+
+    builder
+      .addCase(getTransactionAction.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getTransactionAction.fulfilled, (state, action) => {
+        state.loading = false;
+        state.transaction = action.payload;
+        state.error = null;
+      })
+      .addCase(getTransactionAction.rejected, (state, action) => {
+        state.loading = false;
         state.transaction = null;
         state.error = action.payload;
       });
